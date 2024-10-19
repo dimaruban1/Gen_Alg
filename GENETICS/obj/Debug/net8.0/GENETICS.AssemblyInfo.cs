e13b9657fe8d06c6ad9a730773fd64331c6b4ee2
Dima <41615.dima@gmail.com>
2024-10-20 01:06:46 +0300

    gitignore

--- GENETICS/obj/Debug/net8.0/GENETICS.AssemblyInfo.cs
+++ GENETICS/obj/Debug/net8.0/GENETICS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GENETICS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+622950dea92241df69ab86d53866db4471077118")]
 [assembly: System.Reflection.AssemblyProductAttribute("GENETICS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GENETICS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
